a357e4754e9d352b826998baf8cdd35f57de7fa8
satanja <s.a.tanja@student.tue.nl>
2020-08-25 12:28:32 +0200

    Add input reading

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,38 @@
 
 #include <iostream>
 
+void read(Instance& instance)
+{
+    int u;
+    std::cin >> u;
+
+    int fs;
+    std::cin >> fs;
+    instance.families.resize(fs);
+
+    for (int i = 0; i < u; i++)
+    {
+        instance.universe.insert(i);
+    }
+
+    for (int i = 0; i < fs; i++)
+    {
+        int m;
+        std::cin >> m;
+        for (int j = 0; j < m; j++)
+        {
+            int x;
+            std::cin >> x;
+            instance.families[i].insert(x);
+        }
+    }
+}
+
 int main()
 {
-    Instance I;
-    I.universe = {1, 2, 3};
-    I.families.resize(3);
-    I.families[0] = {1, 2};
-    I.families[1] = {2, 3};
-    I.families[2] = {3, 1};
-    Reducer::reduce(I);
+    Instance instance;
+    read(instance);
+    auto kernel = Reducer::reduce(instance);
+    std::cout << kernel.second << std::endl;
     return 0;
 }

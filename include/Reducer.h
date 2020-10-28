56572f5494bc9df8312d915f47b59a6e6b256bee
satanja <s.a.tanja@student.tue.nl>
2020-10-28 11:57:20 +0100

    Move functional code into header

--- include/Reducer.h
+++ include/Reducer.h
@@ -1,13 +1,51 @@
 #include "Instance.h"
 
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
 #include <vector>
 #include <utility>
-#include <array>
-#pragma once
 
+#pragma once
 class Reducer
 {
 public:
-	static std::pair<Instance, std::vector<int>> reduce(Instance& instance);
-	static std::pair<Instance, std::vector<int>> copy_reduce(Instance instance);
+	std::pair<Instance, std::vector<int>> Reducer::reduce(Instance& instance)
+	{
+		std::vector<int> k;
+		bool reduced = false;
+		do
+		{
+			reduced = false;
+
+			bool unique = instance.has_unique_element();
+			if (unique)
+			{
+				std::vector<int> d = instance.include_unique_elements();
+				k.insert(k.end(), d.begin(), d.end());
+				reduced = true;
+				continue;
+			}
+
+			if (instance.remove_equals())
+			{
+				reduced = true;
+				continue;
+			}
+
+			if (instance.remove_subsets())
+			{
+				reduced = true;
+				continue;
+			}
+
+		} while (reduced);
+		return std::make_pair(instance, k);
+	}
+
+	std::pair<Instance, std::vector<int>> Reducer::copy_reduce(Instance instance)
+	{
+		return reduce(instance);
+	}
 };

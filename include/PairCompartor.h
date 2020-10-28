43d7b8882b4dfedda12a65dd77fca2394b11e778
satanja <s.a.tanja@student.tue.nl>
2020-10-28 11:57:27 +0100

    Delete unused code

--- include/PairCompartor.h
+++
@@ -1,7 +0,0 @@
-bool operator<(const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) {
-    return lhs.second < rhs.second;
-}
-
-bool operator>(const std::pair<int, int>& lhs, const std::pair<int, int>& rhs) {
-    return lhs.second > rhs.second;
-}
